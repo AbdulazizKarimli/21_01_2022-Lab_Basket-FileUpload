15aa077b8b26fc737e99726839915871cc814e11
Abdulaziz930 <abdulazizak@code.edu.az>
2022-01-22 20:02:35 +0400

    initial commit

---
+++ FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrontToBack.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using FrontToBack.Models;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.Where(x => x.IsDeleted == false).ToList();
+            var products = _dbContext.Products.Include(x => x.Category).Where(x => x.IsDeleted == false && x.Category.IsDeleted == false).ToList();
+
+            var homeViewModel = new HomeViewModel
+            {
+                Categories = categories,
+                Products = products,
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
